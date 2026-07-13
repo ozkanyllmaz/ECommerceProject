72a2f8c8e6e7eb07ffba4c817616a906a3c9d16c
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-13 18:26:21 +0300

    feat: ProductController eklendi.

---
+++ ECommerceProject.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using ECommerceProject.Application.Abstractions;
+using ECommerceProject.Application.DTOs.Product;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ECommerceProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _productService.GetAllProductListAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] ProductCreateDto model)
+        {
+            var result = await _productService.InsertAsync(model);
+
+            if (result)
+            {
+                return StatusCode(StatusCodes.Status201Created, "Ürün başarıyla eklendi");
+            }
+            return BadRequest("Ürün eklenirken bir hata oluştu");
+        }
+    }
+}
