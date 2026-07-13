63c7838b0ff1c7c79bdac7134b56c5ee61e39084
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-13 17:58:18 +0300

    feat: product dto tanımlamaları yapıldı.

---
+++ ECommerceProject.Application/DTOs/Product/ProductListDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProject.Application.DTOs.Product
+{
+    public class ProductListDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
