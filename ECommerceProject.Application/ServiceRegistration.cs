f2a5863411e4605adb601400ac4bc5647686831d
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-13 18:22:46 +0300

    feat: Application katmanı servislerini IoC Container'a entegre eden yapılandırma oluşturuldu.

---
+++ ECommerceProject.Application/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ECommerceProject.Application.Abstractions;
+using ECommerceProject.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerceProject.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+        }
+    }
+}
