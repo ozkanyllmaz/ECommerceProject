2d1c4d6f2368bc826abbd6eed3d6047a85abab15
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-15 15:35:04 +0300

    feat: IProductService arayüzüne ve somut ProductService yeni metotlar eklendi ve mapper entegrasyonu yapıldı

--- ECommerceProject.Application/Abstractions/IProductService.cs
+++ ECommerceProject.Application/Abstractions/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceProject.Application.DTOs.Product;
+using ECommerceProject.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,11 @@ namespace ECommerceProject.Application.Abstractions
     public interface IProductService
     {
         Task<List<ProductListDto>> GetAllProductListAsync();
+        Task<ProductDetailDto> GetProductById(Guid id);
         Task<bool> InsertAsync(ProductCreateDto model);
+        Task<bool> DeleteProductAsync(Guid id);
+        Task<bool> RestoreProductAsync(Guid id);
+        Task<bool> UpdateProductAsync(Guid id, ProductUpdateDto model);
+        
     }
 }
