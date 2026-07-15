254d141c2fb8eeab145b9dc728276f035ec2cd08
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-15 15:22:25 +0300

    feat: Product entity'si ile DTO'lar arasındaki dönüşümler için AutoMapper profili eklendi

---
+++ ECommerceProject.Application/Mappings/ProductProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ECommerceProject.Application.DTOs.Product;
+using ECommerceProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProject.Application.Mappings
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            // CreateMap<Kaynak, Hedef>();
+
+            // Veri okuma, listeleme
+            // Akış Yönü: Db -> Backend -> Frontend 
+            CreateMap<Product, ProductListDto>();
+            CreateMap<Product, ProductDetailDto>();
+
+            // Veri ekleme, güncelle
+            // Akış Yönü: Frontend -> Backend -> Db
+            CreateMap<ProductUpdateDto, Product>();
+            CreateMap<ProductCreateDto, Product>();
+        }
+    }
+}
