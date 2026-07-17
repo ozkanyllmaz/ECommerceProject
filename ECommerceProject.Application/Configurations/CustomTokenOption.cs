c7ced9ac7bcbcc792567a550a20ed5a5e6700505
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-17 17:50:25 +0300

    config: JWT ayarları için CustomTokenOption sınıfı eklendi, appsettings yapılandırması yapıldı ve connection string ile security key şifrelenerek kaydedildi.

---
+++ ECommerceProject.Application/Configurations/CustomTokenOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProject.Application.Configurations
+{
+    public class CustomTokenOption
+    {
+        public List<string> Audience {  get; set; } // kitle
+        public string Issuer { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+        public string SecurityKey { get; set; }
+
+    }
+}
