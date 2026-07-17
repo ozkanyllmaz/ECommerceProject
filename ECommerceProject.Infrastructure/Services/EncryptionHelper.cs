1b7578b293ecd370aba9240a6993fe9771e2d5e7
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-17 17:54:45 +0300

    feat: projede gizli tutulacak bilgileri Encrypt ve Decrypt edecek service yazıldı ve program.cs de konfigürasyonları yapıldı

---
+++ ECommerceProject.Infrastructure/Services/EncryptionHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ECommerceProject.Infrastructure.Services
+{
+    public static class EncryptionHelper
+    {
+        public static string Encrypt(string plainText, string key)
+        {
+            using Aes aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.GenerateIV(); // random başlangıç noktası (başlatma vektörü)
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using MemoryStream ms = new();
+            // başlangıç noktasından başla
+            ms.Write(aes.IV, 0, aes.IV.Length);
+            using CryptoStream cs = new(ms, encryptor, CryptoStreamMode.Write);
+            using StreamWriter sw = new(cs);
+            sw.Write(plainText);
+            sw.Close();
+
+            return Convert.ToBase64String(ms.ToArray());
+        }
+
+        public static string Decrypt(string cipherText, string key)
+        {
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using Aes aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(key);
+
+            byte[] iv = new byte[16];
+            Array.Copy(buffer, 0, iv, 0, iv.Length);
+            aes.IV = iv;
+
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            // kalan veriyi çözmek için akışı başlatıyoruz
+            using MemoryStream ms = new(buffer, iv.Length, buffer.Length -  iv.Length);
+            using CryptoStream cs = new(ms, decryptor, CryptoStreamMode.Read);
+            using StreamReader sr = new(cs);
+
+            return sr.ReadToEnd();
+        }
+    }
+}
--- ECommerceProject.WebAPI/Program.cs
+++ ECommerceProject.WebAPI/Program.cs
@@ -1,12 +1,47 @@
 using ECommerceProject.Persistance;
 using ECommerceProject.Application;
 using ECommerceProject.Application.Mappings;
+using ECommerceProject.Infrastructure;
+using ECommerceProject.Application.Configurations;
+using ECommerceProject.Infrastructure.Services;
+using ECommerceProject.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+//------------------------------------
+var masterKey = builder.Configuration["EncryptionMasterKey"];
+
+if (string.IsNullOrEmpty(masterKey))
+{
+    throw new InvalidOperationException("MasterKey bulunamadı!");
+}
+
+var tokenOptions = builder.Configuration.GetSection("CustomTokenOption").Get<CustomTokenOption>();
+var encryptedConnectionString = builder.Configuration.GetConnectionString("MsSql");
+
+if (tokenOptions == null || string.IsNullOrEmpty(encryptedConnectionString))
+{
+    throw new InvalidOperationException("Token ayarları veya connection string okunamadı!");
+}
+// şifreleri anlık olarak RAM üzerinde çözüyoruz
+tokenOptions.SecurityKey = EncryptionHelper.Decrypt(tokenOptions.SecurityKey, masterKey);
+var decryptedConnectionString = EncryptionHelper.Decrypt(encryptedConnectionString, masterKey);
+
+// Çözülmüş verileri IoC Container'a (DI) kaydediyoruz.
+builder.Services.AddSingleton(tokenOptions);
+
+builder.Services.AddDbContext<ECommerceDbContext>(options =>
+    options.UseSqlServer(decryptedConnectionString));
+//------------------------------------
+
+
+
 // Add services to the container.
-builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddPersistenceServices(decryptedConnectionString);
 builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -16,6 +51,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddAutoMapper(cfg => cfg.AddMaps(typeof(ProductProfile).Assembly));
+
 
 var app = builder.Build();
 
