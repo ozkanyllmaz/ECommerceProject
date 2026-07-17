c3db38a0ca5b2f461919192d9567f20b8131aa31
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-17 17:52:41 +0300

    feat: boş (data dönmeyen) API cevaplarında kullanılmak üzere NoContentDto eklendi

---
+++ ECommerceProject.Application/DTOs/Common/NoContentDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProject.Application.DTOs.Common
+{
+    // struct çünkü içi boş bir struct belleğin Heap bölgesinde yer kaplamaz ve 
+    // Çöp toplayıcıyı (Garbage Collector) yormaz
+    public struct NoContentDto
+    {
+    }
+}
