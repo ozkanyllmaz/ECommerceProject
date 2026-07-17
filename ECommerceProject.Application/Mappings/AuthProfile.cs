778a51aa3e1c484bb6ec0a971e120c5079c63ab9
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-17 17:43:57 +0300

    config: yetkilendirme (auth) süreçleri için AutoMapper DTO-Entity dönüşümleri yapılandırıldı

---
+++ ECommerceProject.Application/Mappings/AuthProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ECommerceProject.Application.DTOs.Auth;
+using ECommerceProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProject.Application.Mappings
+{
+    public class AuthProfile : Profile
+    {
+        public AuthProfile()
+        {
+            //CreateMap<Kaynak, Hedef>();
+            CreateMap<UserRegisterDto, User>();
+        }
+    }
+}
