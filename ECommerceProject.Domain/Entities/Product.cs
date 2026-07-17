d00bf040b60dbd1b25703036f9fc8e45c409bfe1
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-17 17:19:00 +0300

    fix: poduct içindeki null referans (NRT) uyarıları giderildi

--- ECommerceProject.Domain/Entities/Product.cs
+++ ECommerceProject.Domain/Entities/Product.cs
@@ -7,8 +7,8 @@ namespace ECommerceProject.Domain.Entities
 {
     public class Product : BaseEntity
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; } 
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string? ImageUrl { get; set; }
