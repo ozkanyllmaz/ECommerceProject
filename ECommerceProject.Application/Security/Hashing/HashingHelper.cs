923b660ccbe698347eea4583ba364386de166bcf
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-17 17:51:33 +0300

    feat: şifre hashleme ve hash li şifreyi doğrulama servisi yazıldı

---
+++ ECommerceProject.Application/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ECommerceProject.Application.Security.Hashing
+{
+    public class HashingHelper
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)); // Şifreyi Tuzla harmanla ve hashle
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            // db deki tuzu algoritmaya veriyoruz ki aynı kalıpla şifreleme yapsın.
+            using var hmac = new HMACSHA512(passwordSalt);
+
+            // kullanıcının girdiği düz şifreyi, db deki tuz ile tekrar hashliyoruz.
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            // Ürettiğimiz hash ile db'den gelen hashi byte byte karşılaştırıyoruz.
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
+}
