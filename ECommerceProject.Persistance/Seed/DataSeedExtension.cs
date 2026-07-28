3d3cce219c3d3aa57d45cb1d4c207ffc68f871c3
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-28 12:02:28 +0300

    feat: Sisteme admin eklemek ve User-Role ilişkisi için SeedData eklendi.

---
+++ ECommerceProject.Persistance/Seed/DataSeedExtension.cs
@@ -0,0 +1,79 @@
+﻿using ECommerceProject.Application;
+using ECommerceProject.Application.Exceptions;
+using ECommerceProject.Application.Security.Hashing;
+using ECommerceProject.Domain;
+using ECommerceProject.Domain.Entities;
+using ECommerceProject.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProject.Persistance.Seed
+{
+    public static class DataSeedExtension
+    {
+        public static async Task SeedDatabaseAsync(this IServiceProvider serviceProvider)
+        {
+            // servicelere erişebilmek için scope oluşturulur.
+            using var scope = serviceProvider.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ECommerceDbContext>();
+
+            try
+            {
+                await context.Database.MigrateAsync();
+
+                if (!await context.Roles.AnyAsync())
+                {
+                    await context.Roles.AddRangeAsync(
+                        new Domain.Entities.Role { Id = Guid.NewGuid(), Name = "Admin" },
+                        new Domain.Entities.Role { Id = Guid.NewGuid(), Name = "Manager" },
+                        new Domain.Entities.Role { Id = Guid.NewGuid(), Name = "Customer" }
+                    );
+                    await context.SaveChangesAsync();
+                }
+                const string adminEmail = "dev.ozkanyilmaz@gmail.com";
+
+                if (!await context.Users.AnyAsync(u => u.Email == adminEmail))
+                {
+                    HashingHelper.CreatePasswordHash("Ozkan123*", out byte[] passwordHash, out byte[] passwordSalt);
+
+                    var adminUser = new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Email = adminEmail,
+                        FirstName = "System",
+                        LastName = "Admin",
+                        PasswordHash = passwordHash,
+                        PasswordSalt = passwordSalt,
+                        Status = true
+                    };
+                    await context.Users.AddAsync(adminUser);
+                    await context.SaveChangesAsync();
+
+                    // Admin kullanıcısına Admin rolu atama
+                    var adminRole = await context.Roles.FirstOrDefaultAsync(r => r.Name == "Admin");
+
+                    if (adminRole != null)
+                    {
+                        var userRole = new UserRole
+                        {
+                            UserId = adminUser.Id,
+                            RoleId = adminRole.Id,
+                        };
+                        await context.UserRoles.AddAsync(userRole);
+                        await context.SaveChangesAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new NotFoundException("Seed eklenirken hata oluştu");
+            }
+
+
+        }
+    }
+}
