dc695894ef3c53bae055246ea04be0c0b6231821
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-28 12:07:41 +0300

    feat: Authorize attiribute eklendi.

--- ECommerceProject.WebAPI/Controllers/AuthController.cs
+++ ECommerceProject.WebAPI/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using ECommerceProject.Application.DTOs.Common;
 using ECommerceProject.Application.Features.Auth.Commands.Login;
 using ECommerceProject.Application.Features.Auth.Commands.RefreshTokens;
 using ECommerceProject.Application.Features.Auth.Commands.Register;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,14 +13,17 @@ namespace ECommerceProject.WebAPI.Controllers
     [ApiController]
     public class AuthController : CustomBaseController
     {
+        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterCommandRequest request)
             => CreateActionResultInstance(await Mediator.Send(request));
 
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginCommandRequest request)
             => CreateActionResultInstance(await Mediator.Send(request));
 
+        [AllowAnonymous]
         [HttpPost("refresh-token")]
         public async Task<IActionResult> RefreshToken([FromBody] RefreshTokensCommandRequest request)
             => CreateActionResultInstance(await Mediator.Send(request));
--- ECommerceProject.WebAPI/Controllers/CustomBaseController.cs
+++ ECommerceProject.WebAPI/Controllers/CustomBaseController.cs
@@ -1,5 +1,6 @@
 ﻿using ECommerceProject.Application.DTOs.Common;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,14 +8,35 @@ namespace ECommerceProject.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class CustomBaseController : ControllerBase
     {
         //private IMediator? _mediator;
         protected IMediator Mediator => HttpContext.RequestServices.GetService<IMediator>();
+
+        [NonAction]
         public IActionResult CreateActionResultInstance<T>(CustomResponseDto<T> response)
         {
             // Http 204 (No Content) ise geri boş response döndür.
             if(response.StatusCode == 204)
+            {
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode,
+                };
+            }
+            // Diğer tüm durumlar için (200, 400, 404, 500) için veriyi ve kodu paketle dön.
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+
+        [NonAction]
+        public IActionResult CreateActionResultInstance(CustomResponseDto response)
+        {
+            // Http 204 (No Content) ise geri boş response döndür.
+            if (response.StatusCode == 204)
             {
                 return new ObjectResult(null)
                 {
