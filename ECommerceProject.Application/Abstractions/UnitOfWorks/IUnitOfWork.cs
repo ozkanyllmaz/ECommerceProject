e5b739f13e7dd9d0b09ebbc4adb977bdbee626a9
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-07-30 22:10:56 +0300

    feat: UnitOfWork yapısı kuruldu

---
+++ ECommerceProject.Application/Abstractions/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProject.Application.Abstractions.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ ECommerceProject.Persistance/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceProject.Application.Abstractions.UnitOfWorks;
+using ECommerceProject.Persistance.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProject.Persistance.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ECommerceDbContext _context;
+
+        public UnitOfWork(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
