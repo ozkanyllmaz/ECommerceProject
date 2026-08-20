20c649490e0404eea8656ed45a49786bce4ea83f
Özkan Yılmaz <ozkanyilmaz.dev@gmail.com>
2026-08-20 09:35:00 +0300

    feat: d45a97e4 commitine ek olarak DashboardController eklendi.

---
+++ ECommerceProject.WebAPI/Controllers/DashboardsController.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceProject.Application.Features.Dashboards.Queries.GetDailyRevenue;
+using ECommerceProject.Application.Features.Dashboards.Queries.GetDashboardPieChart;
+using ECommerceProject.Application.Features.Dashboards.Queries.GetDashboardSummary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceProject.WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DashboardsController : CustomBaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetDashboardAnalytic([FromQuery] GetDashboardSummaryQueryRequest request)
+            => CreateActionResultInstance(await Mediator.Send(request));
+
+        [HttpGet]
+        public async Task<IActionResult> GetPieChartDatas([FromQuery] GetDashboardPieChartQueryRequest request)
+            => CreateActionResultInstance(await Mediator.Send(request));
+
+        [HttpGet]
+        public async Task<IActionResult> GetBarChartDatas([FromQuery] GetDailyRevenueQueryRequest request)
+            => CreateActionResultInstance(await Mediator.Send(request));
+    }
+}
